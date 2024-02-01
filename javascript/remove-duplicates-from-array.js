3b58837eea120c29342f979e4c40e476e31118e7
emanuelefavero <emafaux89@gmail.com>
2024-02-01 14:27:18 +0100

    add javascript/remove-duplicates-from-array solution 2

--- javascript/remove-duplicates-from-array.js
+++ javascript/remove-duplicates-from-array.js
@@ -1,5 +1,7 @@
 // * Remove Duplicates from an Array
 
+// TIP: In-place solution
+
 function removeDuplicates(array) {
   let hash = {}
 
@@ -15,6 +17,16 @@ function removeDuplicates(array) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: Built-in solution
+
+function removeDuplicates2(array) {
+  return [...new Set(array)]
+}
+
+// -----------------------------
 // TESTS
 
 console.log(removeDuplicates([1, 2, 3, 2, 4, 1])) // [1, 2, 3, 4]
+console.log(removeDuplicates2([1, 2, 3, 2, 4, 1])) // [1, 2, 3, 4]
