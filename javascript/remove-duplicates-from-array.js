ec6fdeeaac20978c346b2cf432b9b3c451d8f02a
emanuelefavero <emafaux89@gmail.com>
2024-02-01 14:28:49 +0100

    add javascript/remove-duplicates-from-array solution 3

--- javascript/remove-duplicates-from-array.js
+++ javascript/remove-duplicates-from-array.js
@@ -26,7 +26,23 @@ function removeDuplicates2(array) {
 }
 
 // -----------------------------
+// SOLUTION 3
+
+function removeDuplicates3(array) {
+  let result = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (!result.includes(array[i])) {
+      result.push(array[i])
+    }
+  }
+
+  return result
+}
+
+// -----------------------------
 // TESTS
 
 console.log(removeDuplicates([1, 2, 3, 2, 4, 1])) // [1, 2, 3, 4]
 console.log(removeDuplicates2([1, 2, 3, 2, 4, 1])) // [1, 2, 3, 4]
+console.log(removeDuplicates3([1, 2, 3, 2, 4, 1])) // [1, 2, 3, 4]
