b881e291931f2193673c2e4c3aea2a6f56baeb1c
emanuelefavero <emafaux89@gmail.com>
2024-03-09 22:56:35 +0100

    add javascript/implement-math-sqrt

---
+++ javascript/implement-math-sqrt.js
@@ -0,0 +1,31 @@
+// * Implement Math.mySqrt()
+
+// Math.sqrt() helps us getting the square root of a number.
+
+// Can your write your own mySqrt() ? You should return the integer part only, truncating the fraction part.
+
+function mySqrt(x) {
+  if (typeof x !== 'number' || x < 0 || isNaN(x)) return NaN
+  if (x === 0 || x === 1) return x
+
+  let left = 1
+  let right = x
+  let result = 1
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (mid * mid === x) return mid // found the exact square root
+    else if (mid * mid < x) {
+      left = mid + 1
+      result = mid // store the floor of the square root
+    } else {
+      right = mid - 1
+    }
+  }
+
+  // if no exact square root found, return the floor of the square root
+  return result
+}
+
+console.log(mySqrt(9)) // 3
