169aeecd5001c5984afdea127f6641d99226a45d
emanuelefavero <emafaux89@gmail.com>
2024-06-10 21:08:12 +0200

    add Header dark mode

--- react/app/page.tsx
+++ react/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p>Hello</p>
 
       <ul>
         <li>
--- react/components/Header.tsx
+++ react/components/Header.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className='w-full bg-indigo-50 px-2 py-2 mb-4'>
+    <header className='w-full bg-indigo-100 dark:bg-indigo-950 px-2 py-2 mb-4'>
       <Link href='/' className='font-bold uppercase'>
         <Image
           src='/home.svg'
           alt='logo'
           width={24}
           height={24}
-          className='active:scale-95 transition-transform duration-150 hover:scale-105'
+          className='dark:invert active:scale-95 transition-transform duration-150 hover:scale-105'
         />
       </Link>
     </header>
