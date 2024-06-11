e36d874c13b72871b6ccdcb0bcd60dcaeaee65ca
emanuelefavero <emafaux89@gmail.com>
2024-06-11 17:52:54 +0200

    move temperatures to state variable and add input css

--- react/app/display-object-data/page.tsx
+++ react/app/display-object-data/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react'
+
 interface City {
   temperature: string
   humidity: string
@@ -9,25 +11,25 @@ interface Temperatures {
   [key: string]: City
 }
 
-const temperatures: Temperatures = {
-  'New York': {
-    temperature: '22c',
-    humidity: '70%',
-  },
-  'Los Angeles': {
-    temperature: '30c',
-    humidity: '40%',
-  },
-}
-
 export default function Page() {
+  const [temperatures, setTemperatures] = useState<Temperatures>({
+    'New York': {
+      temperature: '22c',
+      humidity: '70%',
+    },
+    'Los Angeles': {
+      temperature: '30c',
+      humidity: '40%',
+    },
+  })
+
   return (
     <>
       <h1>Display object data</h1>
 
       {Object.keys(temperatures).map((city) => (
         <div key={city}>
-          <h2 className='font-bold text-xl'>{city}</h2>
+          <h3 className='font-bold text-xl'>{city}</h3>
           <p>Temperature: {temperatures[city].temperature}</p>
           <p>Humidity: {temperatures[city].humidity}</p>
         </div>
