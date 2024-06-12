06a8ff5d718f7baa19a654908fd9cfb05a033b66
emanuelefavero <emafaux89@gmail.com>
2024-06-12 20:42:01 +0200

    add seconds and format date in /date

--- react/app/date/page.tsx
+++ react/app/date/page.tsx
@@ -13,17 +13,21 @@ export default function Page() {
     return () => clearInterval(timer)
   }, [])
 
+  const formatDate = (number: number): string => String(number).padStart(2, '0')
+
   return (
     <>
       <h1>Working with dates in React</h1>
 
-      <h2>Current Date</h2>
+      <h2 className='font-bold'>Current Date</h2>
       <p>
         {currentDate.getDate()}/{currentDate.getMonth() + 1}/
         {currentDate.getFullYear()}
       </p>
       <p>
-        {currentDate.getHours()}:{currentDate.getMinutes()}
+        {formatDate(currentDate.getHours())}:
+        {formatDate(currentDate.getMinutes())}:
+        {formatDate(currentDate.getSeconds())}
       </p>
     </>
   )
