3d89bfbbc0d4a98b987c44af8629d844ead8d2b3
emanuelefavero <emafaux89@gmail.com>
2024-06-12 21:04:27 +0200

    add days until christmas in /date

--- react/app/date/page.tsx
+++ react/app/date/page.tsx
@@ -15,20 +15,35 @@ export default function Page() {
 
   const formatDate = (number: number): string => String(number).padStart(2, '0')
 
+  const getDaysUntilChristmas = () => {
+    const currentYear = currentDate.getFullYear()
+    const christmas = new Date(currentYear, 11, 25)
+    const timeDifference = christmas.getTime() - currentDate.getTime()
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24))
+
+    return daysDifference
+  }
+
   return (
     <>
       <h1>Working with dates in React</h1>
 
-      <h2 className='font-bold'>Current Date</h2>
-      <p>
-        {currentDate.getDate()}/{currentDate.getMonth() + 1}/
-        {currentDate.getFullYear()}
-      </p>
-      <p>
-        {formatDate(currentDate.getHours())}:
-        {formatDate(currentDate.getMinutes())}:
-        {formatDate(currentDate.getSeconds())}
-      </p>
+      {/* TIP: When working with dates and timestamps in Next.js, the server and client might render slightly different values. If you get an hydration error it might be useful to add a suppressHydrationWarning attribute */}
+      <div suppressHydrationWarning>
+        <h2>Current Date</h2>
+        <p>
+          {currentDate.getDate()}/{currentDate.getMonth() + 1}/
+          {currentDate.getFullYear()}
+        </p>
+        <p>
+          {formatDate(currentDate.getHours())}:
+          {formatDate(currentDate.getMinutes())}:
+          {formatDate(currentDate.getSeconds())}
+        </p>
+
+        <h2>Days until Christmas</h2>
+        <p>{getDaysUntilChristmas()}</p>
+      </div>
     </>
   )
 }
