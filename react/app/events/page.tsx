b997be1651b6e7f9411580e7010bb109215f6bbe
emanuelefavero <emafaux89@gmail.com>
2024-06-13 16:45:18 +0200

    add onSubmit in /events

--- react/app/events/page.tsx
+++ react/app/events/page.tsx
@@ -10,6 +10,12 @@ export default function Page() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setInputValue(e.target.value)
 
+  // onSubmit
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    alert('Form submitted')
+  }
+
   return (
     <>
       <h1>Events</h1>
@@ -36,6 +42,16 @@ export default function Page() {
       />
       {/* TIP: \u00A0 adds a non breaking space to prevent content shifting */}
       <p className='min-h-6'>{inputValue || '\u00A0'}</p>
+
+      <hr className='my-4 border-slate-500 border-opacity-25' />
+
+      {/* onSubmit */}
+      <h2 className='mb-2'>onSubmit</h2>
+      <form onSubmit={handleSubmit}>
+        <button type='submit' className='bg-violet-500'>
+          Submit
+        </button>
+      </form>
 
       <hr className='my-4 border-slate-500 border-opacity-25' />
     </>
