d4c15c0b0aeb8915bf016318ffb3d0352f61bcd9
emanuelefavero <emafaux89@gmail.com>
2024-06-17 23:36:13 +0200

    add /update-state-arrays delete user

--- react/app/update-state-arrays/page.tsx
+++ react/app/update-state-arrays/page.tsx
@@ -10,6 +10,7 @@ export default function Page() {
   const [users, setUsers] = useState<User[]>([{ id: 0, name: 'Walter' }])
   const [inputValue, setInputValue] = useState('')
 
+  // addUser
   const handleAddUser = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!inputValue) return
@@ -18,11 +19,16 @@ export default function Page() {
     setInputValue('')
   }
 
+  // deleteUser
+  const handleDeleteUser = (id: number) => {
+    setUsers(users.filter((user) => user.id !== id))
+  }
+
   return (
     <>
       <h1>Update state arrays</h1>
 
-      <form onSubmit={handleAddUser}>
+      <form onSubmit={handleAddUser} className='mb-2'>
         <h2>Add user</h2>
         <input
           type='text'
@@ -37,7 +43,16 @@ export default function Page() {
       <h2>Users:</h2>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id}>
+            <span className='mr-2'>{user.name}</span>
+            <button
+              aria-label='Delete user'
+              onClick={() => handleDeleteUser(user.id)}
+              className='p-2 bg-transparent text-rose-500 hover:bg-transparent hover:text-rose-400'
+            >
+              x
+            </button>
+          </li>
         ))}
       </ul>
     </>
