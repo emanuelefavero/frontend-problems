6e7894185a4936b92df97f37cc0db732916137a7
emanuelefavero <emafaux89@gmail.com>
2024-06-18 08:08:34 +0200

    add back button component conditional rendering

--- react/components/BackButton.tsx
+++ react/components/BackButton.tsx
@@ -1,10 +1,19 @@
-import { useRouter } from 'next/navigation'
+'use client'
+
+import { useRouter, usePathname } from 'next/navigation'
 
 export default function BackButton() {
+  const pathname = usePathname()
   const router = useRouter()
 
+  if (pathname === '/') return null
+
   return (
-    <button type='button' aria-label='Back' onClick={() => router.back()}>
+    <button
+      aria-label='Back'
+      onClick={() => router.back()}
+      className='p-0 px-2 bg-transparent hover:bg-transparent focus:ring-0 active:scale-95 transition-transform duration-150 hover:scale-105'
+    >
       ←
     </button>
   )
--- react/components/Header.tsx
+++ react/components/Header.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import BackButton from '@/components/BackButton'
 
 export default function Header() {
   return (
-    <header className='w-full bg-indigo-100 dark:bg-indigo-950 px-2 py-2 mb-4'>
-      <Link href='/' className='font-bold uppercase'>
+    <header className='w-full flex bg-indigo-100 dark:bg-indigo-950 px-2 py-2 mb-4'>
+      <Link href='/' className='font-bold uppercase mr-2'>
         <Image
           src='/home.svg'
           alt='logo'
@@ -13,6 +14,7 @@ export default function Header() {
           className='dark:invert active:scale-95 transition-transform duration-150 hover:scale-105'
         />
       </Link>
+      <BackButton />
     </header>
   )
 }
