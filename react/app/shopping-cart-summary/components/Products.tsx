4b67c6e9347cf82112bb60cb788b0be52fd4fa8e
Emanuele Favero <emafaux89@gmail.com>
2025-08-07 14:38:17 +0200

    feat: add Products frontend

--- react/app/shopping-cart-summary/components/Products.tsx
+++ react/app/shopping-cart-summary/components/Products.tsx
@@ -9,8 +9,24 @@ interface Props {
 export default function Component({ products }: Props) {
   return (
     <>
-      <h2 className='text-3xl font-bold'>Products</h2>
-      <code>{JSON.stringify(products)}</code>
+      <h2 className='mb-2 text-3xl font-bold'>Products</h2>
+
+      <ul className='flex flex-col gap-2'>
+        {products.map((product: Product) => (
+          <li
+            key={product.id}
+            className='flex max-w-sm items-center justify-between'
+          >
+            <div>{product.name}</div>
+            <div className='flex items-center gap-2'>
+              <div className='font-semibold'>${product.price}</div>
+              <button className='bg-amber-300 text-sm font-medium text-black hover:bg-amber-200'>
+                Add to Cart
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
