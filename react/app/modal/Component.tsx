a574f067a6a61cb6eea042f1b64e8af332fdef22
Emanuele Favero <emafaux89@gmail.com>
2025-08-13 15:17:05 +0200

    feat: add animations to modal

--- react/app/modal/Component.tsx
+++ react/app/modal/Component.tsx
@@ -14,14 +14,14 @@ export default function Component() {
         <>
           {/* Overlay */}
           <div
-            className='fixed inset-0 z-40 bg-blue-50/20 backdrop-blur-sm transition-opacity dark:bg-blue-950/20'
+            className='animate-fade-in fixed inset-0 z-40 bg-blue-50/20 backdrop-blur-sm transition-opacity dark:bg-blue-950/20'
             aria-hidden='true'
             onClick={() => setOpen(false)} // clicking on the overlay (everywhere outside the modal card) closes the modal
           />
 
           {/* Modal Card */}
           <div
-            className='absolute left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-lg bg-white p-4 shadow-sm shadow-black/30 dark:bg-blue-950 dark:shadow-black'
+            className='animate-appear absolute left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-lg bg-white p-4 shadow-sm shadow-black/30 dark:bg-blue-950 dark:shadow-black'
             role='dialog'
             aria-modal='true'
             aria-hidden={!open}
